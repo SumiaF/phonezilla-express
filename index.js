4dfd87ba51d8d63a9e34af819bddff819fef27a1
Sumia Farooq <sumiafarooq@gmail.com>
2022-03-05 13:02:04 +0100

    create hero route and hero schema, modify http folder to test and post hero data

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ const app = express();
 const port = process.env.PORT || 8080;
 const connectToDB = require("./models");
 const Post = require("./models/Posts");
-const cors = require("cors")
+const Hero = require("./models/Hero");
+
+const cors = require("cors");
 
 // Middleware
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
 });
-app.use(express.static("./public/"))
+app.use(express.static("./public/"));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app
   .route("/posts")
   .get(async (req, res) => {
-    const posts = await Post.find({}).sort({createdAt: -1});
+    const posts = await Post.find({}).sort({ createdAt: -1 });
     // if (posts) {
     res.json(posts);
     // }
@@ -30,11 +32,21 @@ app
   });
 
 app
-.route("/posts/:id")
-.get(async(req, res) => {
-    const post = await Post.find({_id: req.params.id});
-    res.json(post) 
-})
+  .route("/hero")
+  .get(async (req, res) => {
+    const hero = await Hero.find({});
+
+    res.json(hero);
+  })
+  .post(async (req, res) => {
+    const heroData = await Hero.create(req.body);
+    res.send(heroData);
+  });
+
+app.route("/posts/:id").get(async (req, res) => {
+  const post = await Post.find({ _id: req.params.id });
+  res.json(post);
+});
 
 connectToDB().then(() => {
   app.listen(port, console.log("STARTED LISTENING ON PORT " + port));
